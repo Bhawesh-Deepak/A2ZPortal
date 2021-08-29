4c44beb2c39e1c7cf3b55cfdbd8ba3fd4f23e43c
Bhawesh Deepak <Bhawesh Deepak@DESKTOP-KB2SSTK>
2021-08-29 04:38:05 -0700

    Admin Panel UI

---
+++ A2ZAdmin.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace A2ZAdmin.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
