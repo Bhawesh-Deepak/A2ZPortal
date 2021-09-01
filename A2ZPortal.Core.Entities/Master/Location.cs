deee4c7464ebef67095d0278e430f34e4bbea995
sunil kumar sudhansu <sunil@klbs.in>
2021-09-01 12:03:01 +0530

    Property DetailMerge branch 'master' of https://github.com/Bhawesh-Deepak/A2ZPortal

---
+++ A2ZPortal.Core.Entities/Master/Location.cs
@@ -0,0 +1,19 @@
+﻿using A2ZPortal.Core.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2ZPortal.Core.Entities.Master
+{
+    [Table("Location", Schema = "Master")]
+    public class Location:BaseModel<int>
+    {
+        [Required(ErrorMessage ="Location name is required.")]
+        [Display(Prompt ="Enter Display name")]
+        public string LocationName { get; set; }
+    }
+}
