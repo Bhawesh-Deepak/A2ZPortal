b67c2f767384a769cb7dc018c0f8537d01529acc
sunil kumar sudhansu <sunil@klbs.in>
2021-09-01 12:57:15 +0530

    sunil

---
+++ A2ZAdmin.UI/Controllers/Master/LocationMaster.cs
@@ -0,0 +1,82 @@
+﻿using A2ZPortal.Core.Entities.Common;
+using A2ZPortal.Core.Entities.Master;
+using A2ZPortal.Helper;
+using A2ZPortal.Helper.Extension;
+using A2ZPortal.Infrastructure.Repository.GenericRepository;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace A2ZAdmin.UI.Controllers.Master
+{
+    public class LocationMaster : Controller
+    {
+        private readonly IGenericRepository<Location, int> _iModuleGenericRepository;
+
+        public LocationMaster(IGenericRepository<Location, int> iModuleGenericRepository)
+        {
+            _iModuleGenericRepository = iModuleGenericRepository;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Header"] = "Location Master";
+            return View(ViewPageHelper.InstanceHelper.GetPathDetail(nameof(LocationMaster), "LocationMasterIndex"));
+        }
+
+        public async Task<IActionResult> GetDetail()
+        {
+            var response = await _iModuleGenericRepository.GetList(x => x.IsActive == true && x.IsDeleted == false);
+            if (response.ResponseStatus != ResponseStatus.Error)
+            {
+                return PartialView(ViewPageHelper.InstanceHelper.GetPathDetail(nameof(LocationMaster), "LocationMasterDetails"), response.Entities);
+            }
+            Log.Error(response.Message);
+            return PartialView(ViewPageHelper.InstanceHelper.GetPathDetail("Shared", "Error"));
+
+        }
+        public async Task<IActionResult> Create(int id)
+        {
+            var response = await _iModuleGenericRepository.GetSingle(x => x.Id == id);
+
+            return PartialView(ViewPageHelper.InstanceHelper.GetPathDetail(nameof(LocationMaster), "LocationMasterCreate"), response.Entity);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostCreate(Location model)
+        {
+            if (model.Id > 0)
+            {
+                var updateModel = CommonCrudHelper.CommonUpdateCode(model, 1);
+                var updateResponse = await _iModuleGenericRepository.Update(updateModel);
+                if (updateResponse.ResponseStatus != ResponseStatus.Error)
+                {
+                    return Json("Location Updated Successfully !!!");
+                }
+                Log.Error(updateResponse.Message);
+                return Json("Something went wrong Please contact Admin Team !!!");
+            }
+
+            var createModel = CommonCrudHelper.CommonCreateCode(model, 1);
+            var createResponse = await _iModuleGenericRepository.CreateEntity(createModel);
+            if (createResponse.ResponseStatus != ResponseStatus.Error)
+            {
+                return Json("Location  created Successfully !!!");
+            }
+            Log.Error(createResponse.Message);
+            return Json("Something went wrong Please contact Admin Team !!!");
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            var response = await _iModuleGenericRepository.GetSingle(x => x.Id == id);
+            var deleteModel = CommonCrudHelper.CommonDeleteCode(response.Entity, 1);
+            var deleteResponse = await _iModuleGenericRepository.Delete(deleteModel);
+            return Json(deleteResponse.ResponseStatus != ResponseStatus.Error ?
+                "Location  deleted successfully !!!" : "Something went wrong Please contact Admin !!");
+        }
+
+    }
+}
