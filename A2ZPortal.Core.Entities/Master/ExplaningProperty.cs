f89a2da1000349d17f67c7bc86c461698d10eb6f
sunil kumar sudhansu <sunil@klbs.in>
2021-09-04 12:18:11 +0530

    ssssssss

---
+++ A2ZPortal.Core.Entities/Master/ExplaningProperty.cs
@@ -0,0 +1,20 @@
+﻿using A2ZPortal.Core.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2ZPortal.Core.Entities.Master
+{
+    [Table("ExplaningProperty", Schema = "Master")]
+    public class ExplaningProperty : BaseModel<int>
+    {
+        [Required(ErrorMessage = "Explaning Property is Required.")]
+        [Display(Prompt = "Enter Explaning Property.")]
+        public string Name{ get; set; }
+    }
+    
+}
