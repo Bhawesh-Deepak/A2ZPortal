1f95efab9846ac21c596bbb9e6d0637314a8dcda
sunil kumar sudhansu <sunil@klbs.in>
2021-09-09 14:35:17 +0530

    sunil

---
+++ A2ZPortal.Core.Entities/Property/RentCommercialVM.cs
@@ -0,0 +1,16 @@
+﻿using A2ZPortal.Core.ViewModel.PropertyDetail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2ZPortal.Core.Entities.Property
+{
+    public class RentCommercialVM : PropertyBaseModel
+    {
+        public string Nameoftheowner { get; set; }
+        public string OwnersEmaiAddress { get; set; }
+        public string MobileNoOftheOwner { get; set; }
+    }
+}
